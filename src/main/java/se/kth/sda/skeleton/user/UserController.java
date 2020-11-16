d397edca9c9d094757d690a052c368132e549831
dhana <ponugubatidhanalakshmi4444@gmail.com>
2020-11-16 22:17:29 +0100

    Added mappings to the controller

--- src/main/java/se/kth/sda/skeleton/user/UserController.java
+++ src/main/java/se/kth/sda/skeleton/user/UserController.java
@@ -1,22 +1,25 @@
 package se.kth.sda.skeleton.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     UserService userService;
 
 
+    @GetMapping("")
     public User findUserByEmail(String email) {
         return userService.findUserByEmail(email);
     }
 
-
+    @PostMapping("/register")
     public void registrationProcess(User user) {
          userService.register(user);
     }
